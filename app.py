9bb6ac13f35f74daa0c770d65c19f9868eb186a6
mapcool666 <juju23828825@gmail.com>
2025-08-08 00:15:53 +0800

    update app.py

--- app.py
+++ app.py
@@ -138,4 +138,4 @@ def handle_text(event):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=8888)
